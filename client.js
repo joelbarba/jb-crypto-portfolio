31460a0b47fd14dda33dcef779a01d8319b2d4f3
joel.barba <joel.barba.vidal@gmail.com>
2024-03-14 17:37:42 +0100

    Added altcoins and lazy load part

--- client.js
+++ client.js
@@ -1,38 +1,147 @@
 // alias push="sh ~/DEV/SHELL_SCRIPTS/push_crypto_site.sh"
 // git add -A && git commit -m "some trades" && git push origin master
 const holdings = {
-  BTC:   2.50131148, // trezor
-  ETH:   7.00768131, // trezor
+  BTC:   2.50154806, // (2.50131148 trezor + 0.00023658 phoenix)
+  ETH:   7.00779553, // (7.00768131 trezor + 0.00011422 metamask)
+  SOL:   27.49,         // Bitstamp
+  ALGO:  2723.885413,   // Bitstamp
+  DOT:   78.09,         // Bitstamp
+  MATIC: 702.6170939,   // Bitstamp
+  ADA:   923.9995149,   // Bitstamp
+  XRP:   879.10873954,  // Bitstamp
+  LINK:  37.84194528,   // Bitstamp
+  INJ:   16.12,         // Bitstamp
+  AVAX:  12.85870458,   // Bitstamp
+  IMX:   164.52,        // Bitstamp
+  HBAR:  4086.64314034, // Bitstamp
+  RNDR:  56.91,         // Bitstamp
+  KAS:   3415.34759,    // ByBit
+  ATOM:  49.05030507,   // Kraken
+  ICP:   37.20539115,   // Kraken
+  TRX:   3973.23628,    // Kraken
+  ENS:   8.69565217,    // Kraken
+  GRT:   622.6068549,   // Kraken
+  NEAR:  39.13128546,   // Kraken
+  FIL:   22.34636872,   // Kraken
+  ARB:   102.24426,     // Kraken
+  FET:   83.20159747,   // Kraken
+  SUI:   143.15368,     // Kraken
+  JUP:   146.33147,     // Kraken
+  PYTH:  165.03012,     // Kraken
+  CFG:   147.6232654,   // Kraken
+  XTZ:   76.28253242,   // Kraken
+  BONK:  3751782.1,     // Kraken
+  DYM:   15.90963,      // Kraken
+  TIA:   6.72454,       // Kraken
+  MINA:  82.44023083,   // Kraken
+  AAVE:  0.85019554,    // Kraken
+  OP:    45.30484,      // Kraken
   USDT:  0,
   EUR:   0,
-  ATOM:  49.05030507,   // 26.27370000,   // Alts sold for 0.0623711 BTC
-  ALGO:  2723.885413,   // 1418.40200000,
-  DOT:   78.09,         // 48.65130000,
-  MATIC: 702.6170939,   // 269.43030000,
-  ADA:   923.9995149,   // 551.14830000,
-  SOL:   27.49,         // 9.20079000,
-  XRP:   879.10873954,  // 574.425,
-  LINK:  37.84194528,
-  INJ:   16.12, 
-  AVAX:  12.85870458, 
-
-  // MANA:  0, // 333.66600000,
-  // SAND:  0, // 308.6910000,
-  // ARB:   0, // 96.6033,
-};  
+};
 
 
-
-
-
-const altCoins = () => ([data.ATOM, data.ALGO, data.DOT, data.MATIC, data.ADA, data.SOL, data.XRP, data.LINK, data.INJ, data.AVAX]);
+const altCoins = () => ([
+  data.SOL,   data.ALGO,  data.DOT,   data.MATIC,  data.ADA,  data.XRP,   data.LINK,  data.INJ,
+  data.AVAX,  data.IMX,   data.HBAR,  data.RNDR,   data.KAS,  data.ATOM,  data.ICP,   data.TRX,
+  data.ENS,   data.GRT,   data.NEAR,  data.FIL,    data.ARB,  data.FET,   data.SUI,   data.JUP,
+  data.PYTH,  data.CFG,   data.XTZ,   data.BONK,   data.DYM,  data.TIA,   data.MINA,  data.AAVE,  data.OP,
+]);
 // Object.entries(holdings).forEach(([key, val]) => localStorage.setItem(key, val));
 
 const data = {};
 const totals = { usd: 0, eur: 0, btc: 0 };
-let totalInvested = 80000;
+const investPerCoin = { // Invested EUR per coin
+  BTC:   60230,
+  ETH:   12770,
+  SOL:   2500,
+  ALGO:  500,
+  DOT:   500,
+  MATIC: 500,
+  ADA:   500,
+  XRP:   500,
+  LINK:  500,
+  INJ:   500,
+  AVAX:  500,
+  IMX:   500,
+  HBAR:  500,
+  RNDR:  500,
+  KAS:   500,
+  ATOM:  500,
+  ICP:   500,
+  TRX:   500,
+  ENS:   200,
+  GRT:   200,
+  NEAR:  200,
+  FIL:   200,
+  ARB:   200,
+  FET:   200,
+  SUI:   200,
+  JUP:   100,
+  PYTH:  100,
+  CFG:   100,
+  XTZ:   100,
+  BONK:  100,
+  DYM:   100,
+  TIA:   100,
+  MINA:  100,
+  AAVE:  100,
+  OP:    200,
+};
+let totalInvested = Object.entries(investPerCoin).map(([k,v]) => v).reduce((a, v) => a + v, 0); // 85500
 // const totalInvested = localStorage.getItem('totalInvested') || 50000;
 // localStorage.setItem('totalInvested', totalInvested);
+
+// Altcoins to lazy load (not displayed initially)
+const invisibleRows = ['RNDR','KAS','ATOM','ICP','TRX','ENS','GRT','NEAR','FIL','ARB','FET','SUI','JUP','PYTH','CFG','XTZ','BONK','DYM','TIA','MINA','AAVE','OP'];
+
+
+// <tr class="row-imx">
+//   <td id="imx-holdings">...</td>
+//   <td id="imx-usd-price">...</td>
+//   <td id="imx-eur-price">...</td>
+//   <td id="imx-btc-total">...</td>
+//   <td id="imx-usd-total">...</td>
+//   <td id="imx-eur-total">...</td>
+// </tr>
+// generate Html Table
+const mainTable = document.getElementById('main-table');
+Object.keys(holdings).forEach((name, ind) => {
+  lName = name.toLowerCase();
+  let newRow = mainTable.insertRow(1 + ind);
+  newRow.id = 'row-' + lName;
+  if (invisibleRows.indexOf(name) >= 0) { newRow.classList.add('invisible'); }
+  let newCells = [newRow.insertCell(0), newRow.insertCell(1), newRow.insertCell(2), newRow.insertCell(3), newRow.insertCell(4), newRow.insertCell(5), newRow.insertCell(6)];
+  newCells[0].id = lName + '-holdings';
+  newCells[1].id = lName + '-usd-price';
+  newCells[2].id = lName + '-eur-price';
+  newCells[3].id = lName + '-btc-total';
+  newCells[4].id = lName + '-usd-total';
+  newCells[5].id = lName + '-eur-total';
+  newCells[6].id = lName + '-profit';
+  newCells[6].classList.add('profit-cell');
+  newCells[3].classList.add('coin-btc-total');
+});
+
+showAll = false;
+const loadAllBtn = document.getElementById('load-all-alts');
+if (loadAllBtn) { loadAllBtn.addEventListener('click', () => {
+  showAll = !showAll;
+
+  if (showAll) { mainTable.classList.add('small-table'); }
+  else { mainTable.classList.remove('small-table'); }
+  loadAllBtn.innerText = showAll ? 'Collapse Altcoins' : 'Load All Altcoins';
+  invisibleRows.forEach((name, ind) => {
+    const tr = document.getElementById('row-' + name.toLowerCase());
+    if (showAll) { tr.classList.remove('invisible'); }
+    else { tr.classList.add('invisible'); }
+  });
+
+  loadPrices();
+})}
+
+
+
 
 document.getElementById('btn-copy-clipboard').addEventListener('click', () => copyToClipboard());
 document.getElementById('btn-reload-prices').addEventListener('click', () => loadPrices());
@@ -255,7 +364,7 @@ function checkCurrency(curr) {
   document.getElementById('totals-usd-total').style.display = checkUsd.checked ? 'table-cell' : 'none';
   document.getElementById('totals-eur-total').style.display = checkEur.checked ? 'table-cell' : 'none';
   document.getElementById('totals-profit').style.display = checkEur.checked ? 'table-cell' : 'none';
-  document.getElementById('profit-pad').setAttribute('colspan', checkUsd.checked && checkUsd.checked ? 4 : 2);
+  document.getElementById('profit-pad').setAttribute('colspan', checkUsd.checked && checkUsd.checked ? 5 : 2);
 }
 checkUsd.checked = true;
 checkEur.checked = true;
@@ -269,6 +378,12 @@ document.getElementById('btn-filter-eur')?.addEventListener('click', () => {
   checkEur.checked = true;
   checkCurrency('eur');
 });
+
+
+
+
+
+
 
 async function loadPrices() {
   console.log('Loading prices...');
@@ -293,11 +408,15 @@ async function loadPrices() {
   console.log('----------------');
 
   async function fetchAlt(name, quantity) {
-    if (!document.getElementById(name.toLowerCase() + '-usd-price')) { 
-      console.log('oops');
-    }
-    document.getElementById(name.toLowerCase() + '-usd-price').style.background = '#ffcb0070';
     const obj = { price: { usdt: 0, btc: 0, eur: 0 }, totals: { usd: 0, eur: 0, btc: 0 }};
+
+    if (!showAll && invisibleRows.indexOf(name) >= 0)  { return obj; }
+
+    if (!document.getElementById(name.toLowerCase() + '-usd-price')) { 
+      console.log('oops', name, ' has no html element');
+    } else {
+      document.getElementById(name.toLowerCase() + '-usd-price').style.background = '#ffcb0070';
+    }
     if (quantity > 0) {
       obj.price.usdt = await getPrice(name + 'USDT');
       obj.price.btc = await getPrice(name + 'BTC');   // 1 algo = 0.00001141 btc  => 1 btc = 20900 eur
@@ -306,19 +425,78 @@ async function loadPrices() {
     printCoin(name, quantity, obj);
     return obj;
   }
-  [data.ETH, data.ATOM, data.ALGO, data.DOT, data.MATIC, data.ADA, data.SOL, data.XRP, data.LINK, data.INJ, data.AVAX] = await Promise.all([
+
+  [data.ETH,
+   data.SOL,
+   data.ALGO,
+   data.DOT,
+   data.MATIC,
+   data.ADA,
+   data.XRP,
+   data.LINK,
+   data.INJ,
+   data.AVAX,
+   data.IMX,
+   data.HBAR,
+   data.RNDR,
+   data.KAS,
+   data.ATOM,
+   data.ICP,
+   data.TRX,
+   data.ENS,
+   data.GRT,
+   data.NEAR,
+   data.FIL,
+   data.ARB,
+   data.FET,
+   data.SUI,
+   data.JUP,
+   data.PYTH,
+   data.CFG,
+   data.XTZ,
+   data.BONK,
+   data.DYM,
+   data.TIA,
+   data.MINA,
+   data.AAVE,
+   data.OP,
+   ] = await Promise.all([
     fetchAlt('ETH',   holdings.ETH),
-    fetchAlt('ATOM',  holdings.ATOM),
+    fetchAlt('SOL',   holdings.SOL),
     fetchAlt('ALGO',  holdings.ALGO),
     fetchAlt('DOT',   holdings.DOT),
     fetchAlt('MATIC', holdings.MATIC),
     fetchAlt('ADA',   holdings.ADA),
-    fetchAlt('SOL',   holdings.SOL),
     fetchAlt('XRP',   holdings.XRP),
     fetchAlt('LINK',  holdings.LINK),
     fetchAlt('INJ',   holdings.INJ),
     fetchAlt('AVAX',  holdings.AVAX),
+    fetchAlt('IMX',   holdings.IMX),
+    fetchAlt('HBAR',  holdings.HBAR),
+    fetchAlt('RNDR',  holdings.RNDR),
+    fetchAlt('KAS',   holdings.KAS),
+    fetchAlt('ATOM',  holdings.ATOM),
+    fetchAlt('ICP',   holdings.ICP),
+    fetchAlt('TRX',   holdings.TRX),
+    fetchAlt('ENS',   holdings.ENS),
+    fetchAlt('GRT',   holdings.GRT),
+    fetchAlt('NEAR',  holdings.NEAR),
+    fetchAlt('FIL',   holdings.FIL),
+    fetchAlt('ARB',   holdings.ARB),
+    fetchAlt('FET',   holdings.FET),
+    fetchAlt('SUI',   holdings.SUI),
+    fetchAlt('JUP',   holdings.JUP),
+    fetchAlt('PYTH',  holdings.PYTH),
+    fetchAlt('CFG',   holdings.CFG),
+    fetchAlt('XTZ',   holdings.XTZ),
+    fetchAlt('BONK',  holdings.BONK),
+    fetchAlt('DYM',   holdings.DYM),
+    fetchAlt('TIA',   holdings.TIA),
+    fetchAlt('MINA',  holdings.MINA),
+    fetchAlt('AAVE',  holdings.AAVE),
+    fetchAlt('OP',    holdings.OP),
   ]);
+
 
   calculateTotals();
   console.log(data);
@@ -335,6 +513,7 @@ function calculateTotals() {
   totals.eur = 0;
   totals.btc = 0;
   Object.entries(data).forEach(([key, coin]) => {
+    if (!showAll && invisibleRows.indexOf(key) >= 0)  { return; }
     const quantity = holdings[key];
     coin.totals = {
       usd: Math.round(quantity * coin.price.usdt * 100) / 100,
@@ -349,25 +528,56 @@ function calculateTotals() {
 
 function printValues() {
   printCoin('BTC',  holdings.BTC,   data.BTC);
-  printCoin('ETH',  holdings.ETH,   data.ETH);
-  printCoin('ATOM', holdings.ATOM,  data.ATOM);
-  printCoin('ALGO', holdings.ALGO,  data.ALGO);
-  printCoin('DOT',  holdings.DOT,   data.DOT);
-  printCoin('MATIC',holdings.MATIC, data.MATIC);
-  printCoin('ADA',  holdings.ADA,   data.ADA);
-  printCoin('SOL',  holdings.SOL,   data.SOL);
-  printCoin('XRP',  holdings.XRP,   data.XRP);
-  printCoin('LINK', holdings.LINK,  data.LINK);
-  printCoin('INJ',  holdings.INJ,   data.INJ);
-  printCoin('AVAX', holdings.AVAX,  data.AVAX);
+  printCoin('ETH',   holdings.ETH,    data.ETH);
+  printCoin('SOL',   holdings.SOL,    data.SOL);
+  printCoin('ALGO',  holdings.ALGO,   data.ALGO);
+  printCoin('DOT',   holdings.DOT,    data.DOT);
+  printCoin('MATIC', holdings.MATIC,  data.MATIC);
+  printCoin('ADA',   holdings.ADA,    data.ADA);
+  printCoin('XRP',   holdings.XRP,    data.XRP);
+  printCoin('LINK',  holdings.LINK,   data.LINK);
+  printCoin('INJ',   holdings.INJ,    data.INJ);
+  printCoin('AVAX',  holdings.AVAX,   data.AVAX);
+  printCoin('IMX',   holdings.IMX,    data.IMX);
+  printCoin('HBAR',  holdings.HBAR,   data.HBAR);
+  printCoin('RNDR',  holdings.RNDR,   data.RNDR);
+  printCoin('KAS',   holdings.KAS,    data.KAS);
+  printCoin('ATOM',  holdings.ATOM,   data.ATOM);
+  printCoin('ICP',   holdings.ICP,    data.ICP);
+  printCoin('TRX',   holdings.TRX,    data.TRX);
+  printCoin('ENS',   holdings.ENS,    data.ENS);
+  printCoin('GRT',   holdings.GRT,    data.GRT);
+  printCoin('NEAR',  holdings.NEAR,   data.NEAR);
+  printCoin('FIL',   holdings.FIL,    data.FIL);
+  printCoin('ARB',   holdings.ARB,    data.ARB);
+  printCoin('FET',   holdings.FET,    data.FET);
+  printCoin('SUI',   holdings.SUI,    data.SUI);
+  printCoin('JUP',   holdings.JUP,    data.JUP);
+  printCoin('PYTH',  holdings.PYTH,   data.PYTH);
+  printCoin('CFG',   holdings.CFG,    data.CFG);
+  printCoin('XTZ',   holdings.XTZ,    data.XTZ);
+  printCoin('BONK',  holdings.BONK,   data.BONK);
+  printCoin('DYM',   holdings.DYM,    data.DYM);
+  printCoin('TIA',   holdings.TIA,    data.TIA);
+  printCoin('MINA',  holdings.MINA,   data.MINA);
+  printCoin('AAVE',  holdings.AAVE,   data.AAVE);
+  printCoin('OP',    holdings.OP,     data.OP);
   printCoin('USDT', holdings.USDT,  data.USDT);
   printCoin('EUR',  holdings.EUR,   data.EUR);
 
+  let hiddenInvestment = 0;
+  if (!showAll) {
+    hiddenInvestment = Object.entries(investPerCoin).filter(([k,v]) => invisibleRows.indexOf(k) >= 0).reduce((a, v) => a + v[1], 0);
+    console.log(`There are ${hiddenInvestment} € invested but not displayed`);
+    document.getElementById('hidden-inv-msg').innerHTML = `* There are ${hiddenInvestment} € invested but not displayed`;
+    // totals.eur += hiddenInvestment;
+  }
+
   document.getElementById('totals-btc').innerHTML = ` ~ ${num(totals.btc, 16, 12)} BTC`;
   document.getElementById('totals-usd-total').innerHTML = `${num(totals.usd)} $`;
-  document.getElementById('totals-eur-total').innerHTML = `<span class="totals-eur">${num(totals.eur)}</span> €`;
+  document.getElementById('totals-eur-total').innerHTML = `<span class="totals-eur">${num(totals.eur)}</span> €`;  
 
-  const net = totals.eur - totalInvested;
+  const net = totals.eur - (totalInvested - hiddenInvestment);
   const netClass = net > 0 ? 'pos' : 'neg' ;
   document.getElementById('totals-profit').innerHTML = `<span class="totals-profit ${netClass}">${net > 0 ? '+': ''}${num(net)}</span> €`;
 
@@ -387,23 +597,45 @@ function printValues() {
   const afterTax = net - taxes;
 
   document.getElementById('totals-taxes').innerHTML = `<span>-${num(taxes)} €</span>`;
+  document.getElementById('totals-after-taxes').innerHTML = `<span class="totals-eur">${num(totals.eur - taxes)}</span> €`;
   document.getElementById('totals-profit-after-taxes').innerHTML = `<span class="totals-profit">${afterTax > 0 ? '+': ''}${num(afterTax)}</span> €`;
 
 
 }
 
+
+
+
 function printCoin(coinName, val, obj) {
   const decimals = (coinName === 'BTC' || coinName === 'ETH') ? 2 : 6;
   const coinLC = coinName.toLowerCase();
   const pad5Coin = rPad(coinName.toUpperCase(), 6);
+
   if (!document.getElementById(coinLC + '-holdings')) {
     console.log('oops');
-  }  
+    return;
+  }
   document.getElementById(coinLC + '-holdings').innerHTML = `${pad(val)} ${pad5Coin}`;
   document.getElementById(coinLC + '-usd-price').innerHTML = ` 1 ${pad5Coin} = <span class="usd-price">${num(obj.price.usdt, 10, decimals)}</span> $`;
   document.getElementById(coinLC + '-eur-price').innerHTML = ` 1 ${pad5Coin} = <span class="eur-price">${num(obj.price.eur, 10, decimals)}</span> €`;
   document.getElementById(coinLC + '-usd-total').innerHTML = `<span class="usd-total">${num(obj.totals.usd)}</span> $`;
   document.getElementById(coinLC + '-eur-total').innerHTML = `<span class="eur-total">${num(obj.totals.eur)}</span> €`;
+
+  if (coinName !== 'EUR' && coinName !== 'USDT') {
+    const profit = obj.totals.eur - investPerCoin[coinName];
+    const profitEl = document.getElementById(coinLC + '-profit');
+    if (profit < 0) {
+      profitEl.classList.remove('profit-green');
+      profitEl.classList.add('profit-red');
+      profitEl.innerHTML = `${num(profit, 0)} €`;
+    } else {
+      profitEl.classList.add('profit-green');
+      profitEl.classList.remove('profit-red');
+      profitEl.innerHTML = `+ ${num(profit, 0)} €`;
+    }
+  }
+  
+
   const totalInBtcEl = document.getElementById(coinLC + '-btc-total');
   if (totalInBtcEl) { totalInBtcEl.innerHTML = `${num(obj.totals.btc, 1, 4)} btc`; }
   // if (totalInBtcEl) { totalInBtcEl.innerHTML = `${num(Math.round(obj.totals.btc * 10000) / 10000, 16, 12)}`; }
@@ -482,16 +714,39 @@ function copyToClipboard() {
   let text = `\tUSD\tEUR\tBTC\tUSD\tEUR\tBTC\n`;
   text += 'BTC'  + printLine(data.BTC);
   text += 'ETH'  + printLine(data.ETH);
-  text += 'ATOM' + printLine(data.ATOM);
+  text += 'SOL'  + printLine(data.SOL);
   text += 'ALGO' + printLine(data.ALGO);
   text += 'DOT'  + printLine(data.DOT);
   text += 'MATIC'+ printLine(data.MATIC);
   text += 'ADA'  + printLine(data.ADA);
-  text += 'SOL'  + printLine(data.SOL);
   text += 'XRP'  + printLine(data.XRP);
   text += 'LINK' + printLine(data.LINK);
   text += 'INJ'  + printLine(data.INJ);
   text += 'AVAX' + printLine(data.AVAX);
+  text += 'IMX'  + printLine(data.IMX);
+  text += 'HBAR' + printLine(data.HBAR);
+  text += 'RNDR' + printLine(data.RNDR);
+  text += 'KAS'  + printLine(data.KAS);
+  text += 'ATOM' + printLine(data.ATOM);
+  text += 'ICP'  + printLine(data.ICP);
+  text += 'TRX'  + printLine(data.TRX);
+  text += 'ENS'  + printLine(data.ENS);
+  text += 'GRT'  + printLine(data.GRT);
+  text += 'NEAR' + printLine(data.NEAR);
+  text += 'FIL'  + printLine(data.FIL);
+  text += 'ARB'  + printLine(data.ARB);
+  text += 'FET'  + printLine(data.FET);
+  text += 'SUI'  + printLine(data.SUI);
+  text += 'JUP'  + printLine(data.JUP);
+  text += 'PYTH' + printLine(data.PYTH);
+  text += 'CFG'  + printLine(data.CFG);
+  text += 'XTZ'  + printLine(data.XTZ);
+  text += 'BONK' + printLine(data.BONK);
+  text += 'DYM'  + printLine(data.DYM);
+  text += 'TIA'  + printLine(data.TIA);
+  text += 'MINA' + printLine(data.MINA);
+  text += 'AAVE' + printLine(data.AAVE);
+  text += 'OP'   + printLine(data.OP);
   text += `USDT\t${data.USDT.price.usdt}\t${data.USDT.price.eur}\n`;
   text += `EUR\t${data.EUR.price.usdt}\t${data.EUR.price.eur}\n`;
   navigator.clipboard.writeText(text);
@@ -532,15 +787,26 @@ async function getPrice(symbol) {
   try {
     // const bitstampAlts = ['ALGOUSDT','DOTUSDT','MATICUSDT','ADAUSDT','SOLUSDT','XRPUSDT','LINKUSDT','INJUSDT','AVAXUSDT'];
     // if (bitstampAlts.indexOf(symbol) >= 0) {
+    //   res = await fetch(`https://www.bitstamp.net/api/v2/ticker/algousd`).then(r => r.json());
     //   res = await fetch(`https://www.bitstamp.net/api/v2/ticker/${symbol.slice(0, -1).toLowerCase()}`).then(r => r.json());
     //   return Number.parseFloat(res.last);
     // }
+
+    // Non listed
+    if (symbol === 'KASUSDT')  { return 0.146; }
+    if (symbol === 'KASBTC')   { return 0.000001985; }
+    if (symbol === 'CFGUSDT')  { return 0.7357; }
+    if (symbol === 'CFGBTC')   { return 0.000010141; }
+    if (symbol === 'BONKUSDT') { return 0.00003511; }
+    if (symbol === 'BONKBTC')  { return 0.0000000005; }
+    if (symbol === 'JUPUSDT')  { return 0.9253; }
+    if (symbol === 'JUPBTC')   { return 0.000012754; }
 
     res = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`).then(r => r.json());
     return Number.parseFloat(res.price);
 
   } catch(err) {
-    console.log(err);
+    console.error(symbol);
   }
 }
 
