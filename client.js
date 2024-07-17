6145ae8e82acbecd4d4fafac43b2c1d8b1cabbcf
joel.barba <joel.barba.vidal@gmail.com>
2024-07-17 09:39:00 +0200

    button width + sats

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // alias push="sh ~/DEV/SHELL_SCRIPTS/push_crypto_site.sh"
 // git add -A && git commit -m "some trades" && git push origin master
 const holdings = {
-  BTC:   2.63388599,
+  BTC:   2.67112993,
   ETH:   7.00779553,
   SOL:   27.49,
   ALGO:  2725.58307434,
@@ -44,7 +44,7 @@ const holdings = {
   GALA:  1759.88835,
   NOS:   52.241336,
   USDT:  0,
-  EUR:   0,
+  EUR:   2000,
 };
 
 
@@ -60,7 +60,7 @@ const altCoins = () => ([
 const data = {};
 const totals = { usd: 0, eur: 0, btc: 0 };
 const investPerCoin = { // Invested EUR per coin
-  BTC:   68000,
+  BTC:   70000,
   ETH:   13000,
   SOL:   2500,
   ALGO:  500,
